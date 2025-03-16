4c5cb1c34912583797f61e7d7210b495a5ba7cb8
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2025-03-16 10:39:18 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,1 +1,18 @@
 
+from .admin.auto_delete import auto_delete_command
+from .admin.broadcast import broadcast_command
+from .admin.stats import stats_command
+from .admin.upload import upload_command
+from .user.start import start_command
+from .user.help import help_command
+from .user.about import about_command
+
+__all__ = [
+    'auto_delete_command',
+    'broadcast_command',
+    'stats_command',
+    'upload_command',
+    'start_command',
+    'help_command',
+    'about_command'
+]
